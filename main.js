c37c0b5bf1d982bf8cdd2c0dc1d8ad6cd005e78c
Alessandro Vinci <alessandrovinciabc@gmail.com>
2020-08-18 21:41:53 +0200

    Add entries to array

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 var dataController = (function () {
-    
+    //1. Store data in an array
+    var entriesArr = [];
+
+    var StudyEntry = function (desc) {
+        this.desc = desc;
+        this.date = new Date();
+    };
+
+    return {
+        addItem: function (newItem) {
+            entriesArr.push(new StudyEntry(newItem));
+        },
+
+        getItems: function () {
+            return entriesArr;
+        },
+    };
 })();
 
 var UIController = (function () {
-
     var DOM = {
         addEntryText: "#desc",
         addEntryBtn: ".entry-confirm",
@@ -13,40 +28,60 @@ var UIController = (function () {
     return {
         DOM: DOM,
 
-        getForm: function(){
+        getForm: function () {
             var newItem;
             newItem = document.querySelector(DOM.addEntryText).value;
-            console.log(newItem);
+
+            return newItem;
         },
 
-        clearForm: function(){
-            document.querySelector(DOM.addEntryText).value = '';
-        }
-    } 
+        clearForm: function () {
+            document.querySelector(DOM.addEntryText).value = "";
+        },
+    };
 })();
 
 var controller = (function (data, ui) {
     var DOM = ui.DOM;
 
-    var setupEventListeners = function(){
-        document.querySelector(DOM.addEntryBtn).addEventListener('click', function(){
-            ui.getForm();
-            ui.clearForm();
+    var setupEventListeners = function () {
+        document
+            .querySelector(DOM.addEntryBtn)
+            .addEventListener("click", function () {
+                addItem();
+            });
+
+        document
+            .querySelector(DOM.addEntryCancel)
+            .addEventListener("click", function () {
+                ui.clearForm();
+            });
+
+        document.addEventListener("keydown", function (event) {
+            if (
+                document.activeElement ===
+                document.querySelector(DOM.addEntryText)
+            ) {
+                if (event.key === "Enter") {
+                    addItem();
+                }
+            }
         });
+    };
 
-        document.querySelector(DOM.addEntryCancel).addEventListener('click', function(){
-            ui.clearForm();
-        })
+    var addItem = function () {
+        var newEntry = ui.getForm();
+        data.addItem(newEntry);
+        ui.clearForm();
 
-    }
+        console.log(data.getItems()); //debug
+    };
 
-    return{
-
-        init: function(){
+    return {
+        init: function () {
             setupEventListeners();
         },
     };
-
 })(dataController, UIController);
 
 controller.init();
