381a551767e84dbf205e79a6801632c21c8e7318
Alessandro Vinci <alessandrovinciabc@gmail.com>
2020-08-19 21:31:45 +0200

    Display entries when added

--- main.js
+++ main.js
@@ -2,14 +2,40 @@ var dataController = (function () {
     //1. Store data in an array
     var entriesArr = [];
 
-    var StudyEntry = function (desc) {
+    var StudyEntry = function (desc, id) {
+        this.id = id;
         this.desc = desc;
         this.date = new Date();
+        this.reviewsLeft = 9; //1h, 1d, 3d, 7d, 14d, 21d, 28d, 2m, 3m
     };
+
+    StudyEntry.prototype.getDateString = function(){
+        var day, month, year,
+        months;
+
+        months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+        day = this.date.getDate();
+
+        month = this.date.getMonth();
+        month = months[month];
+
+        year = this.date.getFullYear();
+
+        return day + ' ' + month + ' ' + year;
+    }
 
     return {
         addItem: function (newItem) {
-            entriesArr.push(new StudyEntry(newItem));
+            var newId, lastEntryId;
+
+            if(entriesArr.length > 0){
+                lastEntryId = entriesArr[entriesArr.length - 1].id;
+                newId = lastEntryId + 1;
+            } else {
+                newId = 0;
+            }
+            entriesArr.push(new StudyEntry(newItem, newId));
         },
 
         getItems: function () {
@@ -20,9 +46,10 @@ var dataController = (function () {
 
 var UIController = (function () {
     var DOM = {
-        addEntryText: "#desc",
-        addEntryBtn: ".entry-confirm",
-        addEntryCancel: ".entry-cancel",
+        addEntryText: '#desc',
+        addEntryBtn: '.entry-confirm',
+        addEntryCancel: '.entry-cancel',
+        entriesContainer: '.container-entries > .container-elements',
     };
 
     return {
@@ -38,6 +65,25 @@ var UIController = (function () {
         clearForm: function () {
             document.querySelector(DOM.addEntryText).value = "";
         },
+
+        displayEntry: function(entryToDisplay){
+            var newEntryTemplate, newEntry, DOMentries;
+
+            DOMentries =  document.querySelector(DOM.entriesContainer);
+
+            newEntryTemplate = '<div class="entry-element entry-%entryId%"><div class="element-text"><span class="entry-info">Last Review: %date% - %reviews% more left<br></span>%desc%</div><div class="entry-buttons"><button class="info-btn"><i class="fas fa-info"></i></button><button class="delete-btn"><i class="fas fa-trash-alt"></i></button></div></div>';
+
+            newEntry = newEntryTemplate.replace('%entryId%', entryToDisplay.id);
+            newEntry = newEntry.replace('%desc%', entryToDisplay.desc);
+            newEntry = newEntry.replace('%date%', entryToDisplay.getDateString());
+            newEntry = newEntry.replace('%reviews%', entryToDisplay.reviewsLeft);
+
+            DOMentries.insertAdjacentHTML('afterbegin', newEntry);
+        },
+
+        updateToday: function(){
+
+        }
     };
 })();
 
@@ -47,22 +93,22 @@ var controller = (function (data, ui) {
     var setupEventListeners = function () {
         document
             .querySelector(DOM.addEntryBtn)
-            .addEventListener("click", function () {
+            .addEventListener('click', function () {
                 addItem();
             });
 
         document
             .querySelector(DOM.addEntryCancel)
-            .addEventListener("click", function () {
+            .addEventListener('click', function () {
                 ui.clearForm();
             });
 
-        document.addEventListener("keydown", function (event) {
+        document.addEventListener('keydown', function (event) {
             if (
                 document.activeElement ===
                 document.querySelector(DOM.addEntryText)
             ) {
-                if (event.key === "Enter") {
+                if (event.key === 'Enter') {
                     addItem();
                 }
             }
@@ -71,10 +117,14 @@ var controller = (function (data, ui) {
 
     var addItem = function () {
         var newEntry = ui.getForm();
-        data.addItem(newEntry);
-        ui.clearForm();
 
-        console.log(data.getItems()); //debug
+        if(newEntry !== ''){
+            data.addItem(newEntry);
+            ui.clearForm();
+            ui.displayEntry(data.getItems()[data.getItems().length - 1]);
+    
+            console.log(data.getItems()); //debug
+        }
     };
 
     return {
